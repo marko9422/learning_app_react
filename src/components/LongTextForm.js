5e720d6d05e3627d0c51434848619689593ea75e
Marko Sarcevic <sarcevic.m@outlook.com>
2023-08-19 14:32:45 -0700

    Created form which is working quite well

--- src/components/LongTextForm.js
+++ src/components/LongTextForm.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import { convertToHTML } from 'draft-convert';
 
-export default function LongTextForm() {
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import './LongTextForm.css';
+
+function LongTextForm() {
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+  const [convertedContent, setConvertedContent] = useState(null);
+
+  useEffect(() => {
+    let html = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(html);
+  }, [editorState]);
+
+  console.log(convertedContent);
+
   return (
-    <div>LongTextForm</div>
+    <div className="App">
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={setEditorState}
+        wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+      />
+    </div>
   )
 }
 
+export default LongTextForm
+
+
+// import './../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
